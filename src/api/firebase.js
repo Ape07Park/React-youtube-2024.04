c72dcd2142a5ba78ecbc637721898f85d9854b48
Daniel Park <ape072399@gmail.com>
2024-04-18 17:31:29 +0900

    youtube

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,33 +1,38 @@
 import { initializeApp } from "firebase/app";
-import { GithubAuthProvider } from "firebase/auth";
-import { getAuth, createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
-import { signOut,signInWithEmailAndPassword, onAuthStateChanged, updateProfile } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, GithubAuthProvider, GoogleAuthProvider,
+  signInWithPopup, signOut, updateProfile, signInWithEmailAndPassword,
+  onAuthStateChanged, signInWithRedirect } from "firebase/auth";
+import { getDatabase, ref, get, set } from "firebase/database";
+import { v4 as uuid } from 'uuid';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const database = getDatabase(app);
 
-export function register({email, password, name, photo}){ //  사용처에서 obj로 처리하기에 그것에 맞춰서 제공 
-  console.log('firebase:register():', email, password, photo );
-  createUserWithEmailAndPassword(auth, email, password, photo) // 비동기로 인해 다른 것이 먼저 실행되는 것을 막기 위해 return 함
-  
-  .then(() =>{
-    updateProfile(auth.currentUser, {
-      displayName: name, photoURL: photo
+export function register({ email, password, name, photo }) { //  사용처에서 obj로 처리하기에 그것에 맞춰서 제공 
+
+  console.log('firebase:register():', email, password);
+  createUserWithEmailAndPassword(auth, email, password) // 비동기로 인해 다른 것이 먼저 실행되는 것을 막기 위해 return 함
+
+    .then(() => {
+      updateProfile(auth.currentUser, {
+        displayName: name, photoURL: photo
+      })
     })
-  })
-  .then(() => {logout()})
-  .catch(console.error);
+    .then(() => {logout()})
+    .catch(console.error);
 }
 
 export function login({ email, password }) {
   console.log('firebase.js:login(): ', email, password);
-  signInWithEmailAndPassword(auth, email, password) // email, password 받기
+  signInWithEmailAndPassword(auth, email, password)  // email, password 받기
     .catch(console.error);
 }
 
@@ -37,12 +42,85 @@ export function loginWithGithub() {
     .catch(console.error);
 }
 
+export function loginWithGoogle() {
+  const provider = new GoogleAuthProvider();
+  signInWithRedirect(auth, provider)
+  // signInWithPopup(auth, provider)
+    .catch(console.error);
+}
+
 export function logout() {
   signOut(auth).catch(console.error);
 }
 
 export function onUserStateChanged(callback) {
-  onAuthStateChanged(auth, (user) => {
-    callback(user);
+  onAuthStateChanged(auth, async (user) => {
+    const updatedUser = user ? await adminUser(user) : null;
+    callback(updatedUser);
   });
+}
+
+async function adminUser(user) {
+  return get(ref(database, 'admins'))
+    .then(snapshot => {
+      if (snapshot.exists()) {
+        const admins = snapshot.val();
+        console.log(admins);
+        const isAdmin = admins.includes(user.uid);
+        return {...user, isAdmin};
+      }
+      return user;
+    });
+}
+
+export async function addWatchVideoRecord({ user, video }) {
+  const id = uuid();
+  return set(ref(database, `videoRecords/${id}`), {
+    id, userId:user.uid, userName:user.displayName,
+    videoId:video.id, title:video.snippet.title, channel:video.snippet.channelTitle,
+    thumbnailUrl: video.snippet.thumbnails.medium.url, 
+    watchAt: new Date().toISOString()
+  });
+}
+
+export async function getWatchVideoRecord(userId) {
+  return get(ref(database, 'videoRecords'))
+    .then(snapshot => {
+      if (snapshot.exists()) {
+        const objects = snapshot.val();
+        let records = Object.values(objects);   // object를 array로 변환
+        records = records.filter(record => record.userId === userId);   // userId 필터링
+        records = records.sort((a, b) => b.watchAt.localeCompare(a.watchAt));   // 내림차순 정렬
+        const newRecords = records.filter((record, idx) => {    // 중복 제거
+          return (
+            records.findIndex(eachRecord => {
+              return record.videoId === eachRecord.videoId
+            }) === idx
+          ) 
+        });
+        return newRecords;
+      }
+      return null;
+    });
+}
+
+export async function getTotalWatchVideoRecordByUser() {
+  return get(ref(database, 'videoRecords'))
+    .then(snapshot => {
+      if (snapshot.exists()) {
+        const objects = snapshot.val();
+        let records = Object.values(objects);   // object를 array로 변환
+        records = records.sort((a, b) => b.watchAt.localeCompare(a.watchAt));   // 내림차순 정렬
+        const newRecords = records.filter((record, idx) => {    // 중복 제거
+          return (
+            records.findIndex(eachRecord => {
+              return (record.videoId === eachRecord.videoId && record.userId === eachRecord.userId)
+            }) === idx
+          ) 
+        });
+        const result = Object.groupBy(newRecords, ({ userName }) => userName);    // Grouping
+        return result;
+      }
+      return null;
+    });
 }
--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -29,17 +29,17 @@ export default function SearchHeader() {
 
   return (
     <header>
-      <Stack direction={'row'} sx={{alignItems: 'center'}}>
+      <Stack direction={'row'} alignItems='center'>
         <Grid container>
           <Grid item xs={3}>
             <Link to='/' style={{textDecoration: 'none'}}>
-              <Stack direction={'row'} spacing={1}>
+              <Stack direction={'row'} spacing={1} alignItems='center'>
                 <YouTubeIcon color='error' fontSize="large" />
                 <Typography variant="h4" sx={{fontWeight: 'bold'}}>Youtube</Typography>
               </Stack>
             </Link>
           </Grid>
-          <Grid item xs={5}>
+          <Grid item xs={4}>
             <Paper
               component="form" onSubmit={handleSubmit}
               sx={{ p:'2px 4px', display:'flex', alignItems:'center', width:'100%' }}
@@ -56,15 +56,18 @@ export default function SearchHeader() {
               </IconButton>
             </Paper>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={5}>
             <Stack direction='row' spacing={2} justifyContent='right' alignItems='center'>
-              {user && <Link to='/videos/record'>시청기록</Link>}
+              {user && user.isAdmin && 
+                <Link to='/videos/admin'><Typography variant="h6">관리자 메뉴</Typography></Link>}
+              {user && 
+                <Link to='/videos/record'><Typography variant="h6">시청기록</Typography></Link>}
               {user && user.photoURL && (
                 <img src={user.photoURL} alt={user.displayName} height='32' style={{borderRadius:100}} />
               )}
-              {user && <p>{user.displayName}</p>}
+              {user && <Typography variant="h6">{user.displayName}</Typography>}
               {user && (
-                <Button variant="outlined" onClick={logout} size="small">
+                <Button variant="outlined" onClick={logout}>
                   로그아웃
                 </Button>
               )}
